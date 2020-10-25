6fa940e63ddffc8290040af2c7b51c45aba8209f
xushaoshuai <xu.shaoshuai@cesgroup.com.cn>
2020-10-25 13:44:29 +0800

    client与server端读写代码

--- NettyStudy01/src/main/java/com/xss/netty01/Client.java
+++ NettyStudy01/src/main/java/com/xss/netty01/Client.java
@@ -1,9 +1,14 @@
 package com.xss.netty01;
 
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.ReferenceCountUtil;
+
+import java.lang.ref.Reference;
 
 /**
  * @ClassName Client
@@ -24,7 +29,7 @@ public class Client {
             ChannelFuture f = b.group(group)
                     .channel(NioSocketChannel.class)
                     .handler(new ClientChannelInitializer())
-                    .connect("localhost",8877);
+                    .connect("localhost",8866);
 
             f.addListener(new ChannelFutureListener() {
                 @Override
@@ -38,6 +43,8 @@ public class Client {
             });
             f.sync();
             System.out.println(".......");
+
+            f.channel().closeFuture().sync();
         } finally {
             group.shutdownGracefully();
         }
@@ -47,6 +54,30 @@ class ClientChannelInitializer extends ChannelInitializer{
 
     @Override
     protected void initChannel(Channel ch) throws Exception {
-        System.out.println(ch);
+        ch.pipeline().addLast(new ClientHandler());
+    }
+}
+class ClientHandler extends ChannelInboundHandlerAdapter{
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        //channle 第一次连上可用，写出一个字符串 Direct Memory
+        ByteBuf byteBuf = Unpooled.copiedBuffer("hello".getBytes());
+        ctx.writeAndFlush(byteBuf);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf buf = null;
+        try {
+             buf = (ByteBuf)msg;
+             byte[] bytes = new byte[buf.readableBytes()];
+             buf.getBytes(buf.readerIndex(),bytes);
+             System.out.println(new String(bytes));
+        } finally {
+            if(buf != null){
+                ReferenceCountUtil.release(buf);
+            }
+        }
+
     }
 }
--- NettyStudy01/src/main/java/com/xss/netty01/Server.java
+++ NettyStudy01/src/main/java/com/xss/netty01/Server.java
@@ -1,5 +1,16 @@
 package com.xss.netty01;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.ReferenceCountUtil;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 
@@ -12,11 +23,59 @@ import java.net.ServerSocket;
  * @Return
  */
 public class Server {
-    public static void main(String[] args) throws Exception {
-        ServerSocket ss = new ServerSocket();
-        ss.bind(new InetSocketAddress(8877));
-        ss.accept();
-        System.out.println("a client connect!");
 
+    public static ChannelGroup clients = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+    public static void main(String[] args) throws Exception {
+//        ServerSocket ss = new ServerSocket();
+//        ss.bind(new InetSocketAddress(8877));
+//        ss.accept();
+//        System.out.println("a client connect!");
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(2);
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            ChannelFuture f = b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pl = ch.pipeline();
+                            pl.addLast(new ServerChildHandler());
+                        }
+                    })
+                    .bind(8866)
+                    .sync();
+
+            System.out.println("server started!");
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
+class ServerChildHandler extends ChannelInboundHandlerAdapter{
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        Server.clients.add(ctx.channel());
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = null;
+        try {
+            buf = (ByteBuf)msg;
+            byte[] bytes = new byte[buf.readableBytes()];
+            buf.getBytes(buf.readerIndex(),bytes);
+            System.out.println(new String(bytes));
+            Server.clients.writeAndFlush(msg);
+        }finally {
+//            if(buf != null){
+//                ReferenceCountUtil.release(buf);
+//            }
+        }
     }
 }
